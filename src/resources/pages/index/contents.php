45a4ca46b1ea7ac73395fd82a56e8e6a5819bf03
Narrakas <netkas@users.noreply.github.com>
2019-07-22 19:38:02 -0400

    Designed /index

--- src/resources/pages/index/contents.php
+++ src/resources/pages/index/contents.php
@@ -17,63 +17,112 @@
                 <!-- MAIN CONTENT -->
                 <div class="main-content">
                     <div class="container-fluid">
-                        <h3 class="page-title">Generate Token</h3>
-                        <h4 class="page-title">Lorem ipsum dolor sit amet, <code>consectetur</code> adipiscing elit. Quisque nec venenatis est. Aliquam scelerisque bibendum volutpat. Donec vehicula tincidunt arcu, nec pellentesque neque dignissim eu. Duis a pretium sapien. Suspendisse efficitur eu metus ultrices suscipit. Mauris eget nulla a urna fermentum vulputate. Fusce ac leo rhoncus, convallis sem vel, blandit velit. Vestibulum pharetra dapibus nisi fermentum pretium. </h4>
+                        <h3 class="page-title">Netlenium</h3>
+                        <h4 class="page-title">
+                            Write software that can automate tasks on a web browser without having to worry about Web
+                            Drivers, Browser Compatibility or Operating System Support. Netlenium is designed to be
+                            resource efficient and user-friendly.
+                        </h4>
                         <div class="row">
                             <div class="col-md-7">
-                                <!-- TABLE HOVER -->
                                 <div class="panel">
                                     <div class="panel-heading">
-                                        <h3 class="panel-title">Request</h3>
+                                        <h3 class="panel-title">Download Netlenium</h3>
                                     </div>
                                     <div class="panel-body">
                                         <table class="table table-hover">
                                             <thead>
-                                            <tr>
-                                                <th>Parameter</th>
-                                                <th>Type</th>
-                                                <th>Position</th>
-                                                <th>#</th>
-                                                <th>Description</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            <tr>
-                                                <td>Authorization</td>
-                                                <td><code>string</code></td>
-                                                <td><code>Header</code></td>
-                                                <td><code>Required</code></td>
-                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </td>
-                                            </tr>
-                                            <tr>
-                                                <td>username</td>
-                                                <td><code>string</code></td>
-                                                <td><code>Body</code></td>
-                                                <td><code>Required</code></td>
-                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec venenatis est. Aliquam scelerisque bibendum volutpat. Donec vehicula tincidunt arcu, nec pellentesque neque dignissim eu. </td>
-                                            </tr>
+                                                <tr>
+                                                    <th>Platform</th>
+                                                    <th>Version</th>
+                                                    <th>Download</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <i class="fa fa-windows"></i> Windows x86
+                                                        </td>
+                                                        <td>1.0.0.0</td>
+                                                        <td>
+                                                            <button class="btn btn-sm btn-success"> Download </button>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <i class="fa fa-windows"></i> Windows x64
+                                                        </td>
+                                                        <td>1.0.0.0</td>
+                                                        <td>
+                                                            <button class="btn btn-sm btn-success"> Download </button>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <i class="fa fa-linux"></i> Linux x86
+                                                        </td>
+                                                        <td>1.0.0.0</td>
+                                                        <td>
+                                                            <button class="btn btn-sm btn-success"> Download </button>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <i class="fa fa-linux"></i> Linux x64
+                                                        </td>
+                                                        <td>1.0.0.0</td>
+                                                        <td>
+                                                            <button class="btn btn-sm btn-success"> Download </button>
+                                                        </td>
+                                                    </tr>
                                             </tbody>
                                         </table>
                                     </div>
                                 </div>
-                                <!-- END TABLE HOVER -->
                             </div>
                             <div class="col-md-5">
-                                <!-- TABLE HOVER -->
                                 <div class="panel">
                                     <div class="panel-heading">
-                                        <h3 class="panel-title">Response</h3>
+                                        <h3 class="panel-title">Supported Browsers</h3>
                                     </div>
                                     <div class="panel-body">
-                                        <pre>{}</pre>
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th>Browser</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td><i class="fa fa-chrome"></i> Google Chrome, Dev, Beta & Chromium</td>
+                                                </tr>
+                                                <tr>
+                                                    <td><i class="fa fa-firefox"></i> Firefox, Developer Edition, Nightly & Beta</td>
+                                                </tr>
+                                                <tr>
+                                                    <td><i class="fa fa-opera"></i> Opera 60 & Developer Edition</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
-                                <!-- END TABLE HOVER -->
                             </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="panel">
+                                    <div class="panel-heading">
+                                        <h3 class="panel-title">Screenshot</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <img class="img-responsive" src="/assets/img/netlenium_screenshot.png">
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
-                <!-- END MAIN CONTENT -->
             </div>
             <!-- END MAIN -->
             <div class="clearfix"></div>
