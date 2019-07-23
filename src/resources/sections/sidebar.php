381a193a1e1631a4845d4eac16febf03d48a0588
Narrakas <netkas@users.noreply.github.com>
2019-07-22 20:16:54 -0400

    Updated sidebar

--- src/resources/sections/sidebar.php
+++ src/resources/sections/sidebar.php
@@ -2,149 +2,70 @@
     <div class="sidebar-scroll">
         <nav>
             <ul class="nav">
-                <li><a href="#" class=""><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+                <li><a href="/" class="">
+                        <i class="lnr lnr-home"></i> <span>Introduction</span>
+                    </a>
+                </li>
                 <li>
-                    <a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+                    <a href="#subPages" data-toggle="collapse" class="collapsed">
+                        <i class="lnr lnr-question-circle"></i>
+                        <span>How to use</span>
+                        <i class="icon-submenu lnr lnr-chevron-left"></i>
+                    </a>
                     <div id="subPages" class="collapse ">
                         <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
+                            <li>
+                                <a href="/introduction">How does this work?</a>
+                            </li>
+                            <li>
+                                <a href="/api_wrappers">Netlenium Libraries</a>
+                            </li>
                         </ul>
                     </div>
                 </li>
                 <li>
-                    <a href="#subPages2" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 2</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+                    <a href="#subPages2" data-toggle="collapse" class="collapsed">
+                        <i class="lnr lnr-code"></i>
+                        <span>API Usage</span>
+                        <i class="icon-submenu lnr lnr-chevron-left"></i>
+                    </a>
                     <div id="subPages2" class="collapse ">
                         <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
+                            <li><a href="/api_root">/</a></li>
+                            <li><a href="/api_sessions">/sessions</a></li>
+                            <li><a href="/api_navigation">/navigation</a></li>
+                            <li><a href="/api_actions">/actions</a></li>
+                            <li><a href="/api_web_element">/web_element</a></li>
+                            <li><a href="/api_window_handler">/window_handler</a></li>
+                            <li><a href="/api_admin">/admin</a></li>
                         </ul>
                     </div>
                 </li>
                 <li>
-                    <a href="#subPages3" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 3</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+                    <a href="#subPages3" data-toggle="collapse" class="collapsed">
+                        <i class="lnr lnr-tag"></i>
+                        <span>Objects</span>
+                        <i class="icon-submenu lnr lnr-chevron-left"></i>
+                    </a>
                     <div id="subPages3" class="collapse ">
                         <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
+                            <li><a href="/object_window">Window</a></li>
+                            <li><a href="/object_location">Location</a></li>
+                            <li><a href="/object_size">Size</a></li>
+                            <li><a href="/object_proxy">Proxy</a></li>
+                            <li><a href="/object_server">Server Details</a></li>
+                            <li><a href="/object_session">Session</a></li>
+                            <li><a href="/object_element">Web Element</a></li>
                         </ul>
                     </div>
                 </li>
-                <li>
-                    <a href="#subPages4" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 4</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages4" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
+                <li><a href="/" class="">
+                        <i class="lnr lnr-bug"></i> <span>Bug Report</span>
+                    </a>
                 </li>
-                <li>
-                    <a href="#subPages5" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 5</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages5" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages6" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 6</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages6" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages7" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 7</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages7" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages8" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 8</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages8" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages9" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 9</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages9" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages10" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 10</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages10" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages11" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 11</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages11" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages12" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 12</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages12" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#subPages13" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Token 13</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages13" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="#" class="">Check Token &nbsp; <span class="label label-success">GET</span></a></li>
-                            <li><a href="#" class="">Generate Token &nbsp; <span class="label label-primary">POST</span></a></li>
-                            <li><a href="#" class="">Refresh Token &nbsp; <span class="label label-warning">PUT</span></a></li>
-                            <li><a href="#" class="">Delete Token &nbsp; <span class="label label-danger">DELETE</span></a></li>
-                        </ul>
-                    </div>
+                <li><a href="/" class="">
+                        <i class="lnr lnr-bubble"></i> <span>Support</span>
+                    </a>
                 </li>
             </ul>
         </nav>
