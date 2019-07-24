fb471f445c73ba7727f1f016bb111c24cb801a01
Narrakas <netkas@users.noreply.github.com>
2019-07-24 18:39:33 -0400

    Added /api_web_element

--- src/resources/sections/navbar.php
+++ src/resources/sections/navbar.php
@@ -1,10 +1,16 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="brand">
-        <a href="index.html"><img src="assets/img/logo-dark.png" alt="Klorofil Logo" class="img-responsive logo"></a>
+        <a href="/"><img src="/assets/img/logo-dark.png" alt="Netlenium" class="img-responsive logo"></a>
     </div>
     <div class="container-fluid">
         <div class="navbar-btn">
             <button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
+        </div>
+        <div class="navbar-btn navbar-btn-right">
+            <a class="btn btn-info" href="https://paypal.me/intellivoid" title="Support us" target="_blank">
+                <i class="fa fa-paypal"></i>
+                <span>SUPPORT US</span>
+            </a>
         </div>
     </div>
 </nav>
